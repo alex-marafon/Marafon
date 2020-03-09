d7143d7adaac3b4c314beb2ff6b5043f2b73d5d0
Alex Marafon <alex-asam@hotmail.com>
2020-03-09 02:02:45 -0300

    Editado layout padrao

---
+++ Aluguel/src/Aluguel.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aluguel.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
