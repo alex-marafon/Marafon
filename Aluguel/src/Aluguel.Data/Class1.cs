d7251ea570aff6174bd3307560b8e776827c8329
Alex Marafon <alex.marafonn@gmail.com>
2020-03-09 17:10:18 -0400

    Estrutura ORM e DbContext

--- Aluguel/src/Aluguel.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Aluguel.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ Aluguel/src/Aluguel.Data/ORM/AluguelDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aluguel.Data.ORM
+{
+   public class AluguelDbContext : DbContext
+    {
+        public AluguelDbContext(DbContextOptions<AluguelDbContext> options) : base(options) 
+        {
+        
+        }
+    }
+}
