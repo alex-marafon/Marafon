140d5491405286c667530465021f1265282130b1
Alex Marafon <alex.marafonn@gmail.com>
2020-03-09 11:17:26 -0400

    Criando esturutura para cadastrar Locatario

---
+++ Aluguel/src/Aluguel.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aluguel.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Aluguel/src/Aluguel.Dominio/Entidade/EntitiBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aluguel.Dominio.Entidade
+{
+    public class EntitiBase
+    {
+        public EntitiBase()
+        {
+            this.Id = new Guid();
+        }
+        public Guid Id { get; set; }
+    }
+}
