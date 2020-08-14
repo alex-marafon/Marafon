6187c0891c91f6289814e615f4b9801257ce9dcc
Alex Marafon <alex.marafonn@gmail.com>
2020-08-14 16:30:35 -0400

    Up popula DB

--- Aluguel/src/Aluguel.Data/Migrations/20200812144710_PopulaDb.cs
+++ Aluguel/src/Aluguel.Data/Migrations/20200812144710_PopulaDb.cs
@@ -9,15 +9,20 @@ namespace Aluguel.Data.Migrations
             //Imovel
             //mb.Sql("Insert into Imovel(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status) Values('Ap1','Sinop','MT','Jd Jacarandas','Primaveras',249,78550000,'true')");
             
-            for (int i = 0; i < 5; i++)
-            {
-                mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('testeVS','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
-                i++;
-            }
+          
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('Ap','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('Kitnet5','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('Kitnet1','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('AP2','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('Ap3','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
+            mb.Sql("INSERT INTO Imovels(NomeImovel,Cidade,Estado,Bairro,Rua,Numero,Cep,Status,ContratoId,ImovelId1) VALUES('Kitnet2','Sinop','MT','jd jacarandas','Primaveras','254','354785256','false',null,null)");
 
 
 
             ////Locador
+            mb.Sql("CREATE TABLE Locadores(Nome,SobreNome,Cpf,Rg,OrgaoEmissor,Profissao,EstadoCivil,Email,Tel,TelTestemunha,Status,ContratoId) VALUES( 'Joao','Silva','000000','0000','SSPMT','Trabalh','Solteiro','Joao@gmail.com','01012120','000000','false',null)");
+
+
             //mb.Sql("insert into Locador(Status,Nome,SobreNome,Cpf,Rg,OrgaoEmissor,Profissao,EstadoCivil,Email,Tel,TelTestemunha) " +
             //                   "Values('true','Joao','Silva',0000000000,0000000,'SSSP','Trabalho','Solteiro','joao@gmail.com',35310000, 35310000)");
             //mb.Sql("insert into Locador(Status,Nome,SobreNome,Cpf,Rg,OrgaoEmissor,Profissao,EstadoCivil,Email,Tel,TelTestemunha) " +
